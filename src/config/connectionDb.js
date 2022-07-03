a8e4c1437bc175baa661d1c267c817075285b7b6
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-07-03 23:23:52 +0530

    resumed database

--- src/config/connectionDb.js
+++ src/config/connectionDb.js
@@ -6,7 +6,8 @@ const PASSWORD = "bluemercury2";
 const DATABASE = "bluemercurydb";
 
 // Database connection url
-const DEFAULT_CONNECTION_STRING = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.1bi7u.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+// const DEFAULT_CONNECTION_STRING = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.1bi7u.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+const DEFAULT_CONNECTION_STRING= `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.1bi7u.mongodb.net/${DATABASE}?retryWrites=true&w=majority`
 
 // Mongoose options for connection
 const MONGOOSE_OPTIONS = {
